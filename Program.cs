502b1dba8351dece01fc735f33c4d620c851938f
ElifSueda <elifsuedaunal97@gmail.com>
2021-04-23 19:04:35 +0300

    added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Deneme
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("dsçmjlg");
         }
     }
 }
